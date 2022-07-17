99033dd83fef86d16a0dced6dd79cda5d1692187
Luan F. Silva <luanflorencio87@gmail.com>
2022-07-17 00:06:16 -0300

    fix: render List with a key

--- src/UseState.jsx
+++ src/UseState.jsx
@@ -10,29 +10,39 @@ const BuySystem = styled.div`
         width: 50%;
         display: flex;
         flex-direction: column;
+        align-items: center;
     }
 
     .BuyList {
-        width: 50%;
+        width: 45%;
+        height: 100%;
+        border-radius: 12px;
+        color: #3df;
         text-align: right;
+        ul {
+            padding: 0 20px 0 0;
+            list-style: none;
+        }
     }
 `
 
 export const UseState = () => {
-    const [points, setPoints] = useState(33);
 
-    const handleClick = (e) => {
+    const [points, setPoints] = useState(33);
+    const modifyPoints = (e) => {
         const action = +e.target.textContent.slice(-2);
         setPoints(points + (action));
     }
 
-    const [thing, setThing] = useState([]);
-
-    const insertThing = () => {
-        const fres = document.querySelector('input').value;
-        const itemList = <li key={Math.random()}><code>{fres}</code></li>;
-        setThing([itemList, ...thing]);
+    const [things, setThings] = useState(['Chá', 'Suco de azul']);
+    const insertThings = () => {
+        const newThing = document.querySelector('input').value;
+        (newThing) ? setThings([newThing, ...things]) : null;
     }
+    const resetThings = () => {
+        setThings([]);
+    }
+    const renderList = things.map((thing, i) => <li key={i}>{thing}</li>);
 
     return (
         <>
@@ -40,21 +50,22 @@ export const UseState = () => {
             <p>Hook para criar states <q>estados</q> capaz de manipular e salvar dados, elementos, objetos, arrays...</p>
             <p>O exemplo mais simples que se pode dar é este abaixo, onde pode-se mudar o estado 'points' com os botões:</p>
             <p>points: {points}</p>
-            <button onClick={handleClick}>Aumentar +1</button>
-            <button onClick={handleClick}>Diminuir -1</button>
+            <button onClick={modifyPoints}>Aumentar +1</button>
+            <button onClick={modifyPoints}>Diminuir -1</button>
 
             <hr />
 
-            <p>Outro exemplo é usar o useState com arrays, no caso desse exemplo, vamos fazer uma lista de compras</p>
             <BuySystem>
                 <div className="BuyConfig">
+                    <p>Outro exemplo é usar o useState com arrays, no caso desse exemplo, vamos fazer uma lista de compras</p>
                     <input type="text" placeholder="digite aqui" maxLength={12} />
-                    <button onClick={insertThing}>Inserir</button>
+                    <button onClick={insertThings}>Inserir</button>
+                    {things.length >= 1 ? <button onClick={resetThings}>Resetar</button> : ''}
                 </div>
 
                 <div className="BuyList">
                     <ul>
-                        {thing}
+                        {renderList}
                     </ul>
                 </div>
             </BuySystem>
